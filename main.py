6db6db7111b97ca214ee157a97f5c92514edc16e
Aidan <aidan.fu000@gmail.com>
2021-08-04 00:39:07 -0400

    remove venv file gitignore

--- main.py
+++
@@ -1,6 +0,0 @@
-import scripts.inputmanager as inp
-
-STARTING_MESSAGE = "Before saving new parameters try testing to see the best accuracy I got."
-
-print(STARTING_MESSAGE.upper())
-inp.input_loop()
